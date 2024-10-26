1c9eae7b7e6833a25e92a599f0123d6fff7e0f9e
lulzshadowwalker <lulzshadowwalker@gmail.com>
2024-10-26 19:00:34 +0300

    (nvim) mappings, options and added splitjoin plugin

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -11,10 +11,12 @@ map("n", "<Leader>tv", ":TestVisit<CR>", { desc = "Test Visit" })
 map("n", "ds", function() require("telescope.builtin").lsp_document_symbols() end, { desc = "LSP Document symbols" })
 
 -- Reselect visual selection after indenting.
-map('v', '<', '<gv')
-map('v', '>', '>gv')
+map("v", "<", "<gv")
+map("v", ">", ">gv")
 
-map('n', '<leader>k', ':nohl<CR>', { desc = 'Clear highlights' })
+map("n", "<leader>k", ":nohl<CR>", { desc = "Clear highlights" })
+
+map("n", "<leader>o", ":only<CR>", { desc = "Hide all other windows" })
 
 function Close_others()
   local current = vim.api.nvim_get_current_buf()
@@ -32,3 +34,25 @@ map("n", "<leader>qo", Close_others, { desc = "Close all open buffers except cur
 
 map("n", "<leader>==", "mmggVG=`m", { desc = "Auto indent the whole file" })
 
+
+function Close_hidden_buffers()
+  local current = vim.api.nvim_get_current_buf()
+  local bufs = vim.api.nvim_list_bufs()
+
+  for _, buf in ipairs(bufs) do
+    if current ~= buf and vim.api.nvim_buf_is_loaded(buf) then
+      local is_open = false
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        if vim.api.nvim_win_get_buf(win) == buf then
+          is_open = true
+          break
+        end
+      end
+      if not is_open then
+        vim.cmd("bd" .. buf)
+      end
+    end
+  end
+end
+
+map("n", "<leader>qQ", Close_hidden_buffers, { desc = "Close all hidden buffers" })
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -7,3 +7,16 @@ local o = vim.o
 o.wrap = false
 o.guicursor=''
 o.relativenumber = true
+
+o.scrolloff = 8
+o.sidescrolloff = 8
+
+o.wildmode = 'longest:full,full' -- complete the longest common match, and allow tabbing the results to fully complete them
+o.completeopt = 'menuone,longest,preview'
+
+o.title = true
+
+o.wildmode = 'longest:full,full' -- complete the longest common match, and allow tabbing the results to fully complete them
+o.completeopt = 'menuone,longest,preview'
+
+o.title = true
