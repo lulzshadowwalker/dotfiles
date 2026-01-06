a8fa0d817c0ccc0162a9eb617fff3ff380022ee9
lulzshadowwalker <lulzshadowwalker@gmail.com>
2026-01-06 17:53:52 +0300

    tmux, neovim, and ghosty now play nicely with one dark theme
    clone tpm with depth 1
    
    Signed-off-by: lulzshadowwalker <lulzshadowwalker@gmail.com>

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ ln -sf /Applications/kitty.app/Contents/MacOS/kitty /usr/local/bin/kitty
 rm -rf $HOME/.tmux.conf 
 ln -sf $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm --depth=1
 fi
 
 rm -rf $HOME/.config/nvim
