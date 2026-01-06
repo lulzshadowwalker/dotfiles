392adeff2170aa2356ca37d2e3f29849eef44e95
lulzshadowwalker <lulzshadowwalker@gmail.com>
2026-01-06 14:30:44 +0300

    remove unused configs
    Signed-off-by: lulzshadowwalker <lulzshadowwalker@gmail.com>

--- install.sh
+++ install.sh
@@ -2,39 +2,15 @@
 
 DOTFILES=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
-# rm -rf $HOME/.config/kitty 
-# ln -s $DOTFILES/kitty $HOME/.config/kitty
-# ln -s /Applications/kitty.app/Contents/MacOS/kitty /usr/local/bin/kitty
+rm -rf $HOME/.config/kitty 
+ln -s $DOTFILES/kitty $HOME/.config/kitty
+ln -s /Applications/kitty.app/Contents/MacOS/kitty /usr/local/bin/kitty
 
 rm -rf $HOME/.tmux.conf 
 ln -s $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
 
-mkdir -p $HOME/.local/bin
-
-rm -rf $HOME/.local/bin/t
-ln -s $DOTFILES/scripts/t $HOME/.local/bin/t
-
 rm -rf $HOME/.config/nvim
 ln -s $DOTFILES/nvim $HOME/.config/nvim
 
-rm -rf $HOME/.config/alacritty
-ln -s $DOTFILES/alacritty $HOME/.config/alacritty
-
 rm -rf $HOME/.config/phpactor
 ln -s $DOTFILES/phpactor $HOME/.config/phpactor
-
-rm -f $HOME/.wezterm.lua
-ln -s $DOTFILES/wezterm/wezterm.lua $HOME/.wezterm.lua
-
-rm -rf $HOME/.config/ghostty/config
-mkdir -p $HOME/.config/ghostty
-ln -s $DOTFILES/ghostty/config $HOME/.config/ghostty/config
-
-rm -rf $HOME/.config/nix
-ln -s $DOTFILES/nix $HOME/.config/nix
-
-rm -rf $HOME/.config/fish
-ln -s $DOTFILES/fish $HOME/.config/fish
-
-rm -f $HOME/.config/starship.toml
-ln -s $DOTFILES/starship/starship.toml $HOME/.config/starship.toml
