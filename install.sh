f183e1af1454dc5afef379289fa0764f0a7e9836
lulzshadowwalker <lulzshadowwalker@gmail.com>
2026-01-06 16:02:18 +0300

    Update tmux, kitty, and ghosty configs
    Signed-off-by: lulzshadowwalker <lulzshadowwalker@gmail.com>

--- install.sh
+++ install.sh
@@ -8,9 +8,16 @@ ln -s /Applications/kitty.app/Contents/MacOS/kitty /usr/local/bin/kitty
 
 rm -rf $HOME/.tmux.conf 
 ln -s $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+fi
 
-rm -rf $HOME/.config/nvim
-ln -s $DOTFILES/nvim $HOME/.config/nvim
+# rm -rf $HOME/.config/nvim
+# ln -s $DOTFILES/nvim $HOME/.config/nvim
 
-rm -rf $HOME/.config/phpactor
-ln -s $DOTFILES/phpactor $HOME/.config/phpactor
+# rm -rf $HOME/.config/phpactor
+# ln -s $DOTFILES/phpactor $HOME/.config/phpactor
+
+rm -rf $HOME/.config/ghostty/config
+mkdir -p $HOME/.config/ghostty
+ln -s $DOTFILES/ghostty/config $HOME/.config/ghostty/config
